eb7adaf2640a369be6011fc801dad93e2efc39bc
Vojta Jina <vojta.jina@gmail.com>
2014-02-06 09:54:25 -0800

    Remove symlink if exists, do not create unless in node_modules

--- create-symlinks.js
+++ create-symlinks.js
@@ -4,8 +4,19 @@ var path = require('path');
 var NODE_MODULES_DIR = path.resolve(__dirname, '..');
 var PARENT_DIR = path.resolve(__dirname, '../..');
 
+if (path.basename(NODE_MODULES_DIR) !== 'node_modules') {
+  console.log('Pipe is not inside node_modules, not installing symlinks.')
+  process.exit(0);
+}
 
-// TODO(vojta): handle if the symlink already exists
+
 ['karma', 'gulp'].forEach(function(name) {
-  fs.symlinkSync('pipe/node_modules/' + name, NODE_MODULES_DIR + '/' + name);
+  var symlinkPath = NODE_MODULES_DIR + '/' + name;
+
+  if (fs.existsSync(symlinkPath)) {
+    console.log(symlinkPath + ' already exists, removing...');
+    fs.unlinkSync(symlinkPath);
+  }
+
+  fs.symlinkSync('pipe/node_modules/' + name, symlinkPath);
 });
