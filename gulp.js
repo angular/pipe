9c7749c377b6afd628e37fc736c44908e91afc8d
Vojta Jina <vojta.jina@gmail.com>
2014-02-07 15:22:43 -0800

    chore: fix the commonjs build task

--- gulp.js
+++ gulp.js
@@ -1,7 +1,8 @@
 // Shared Gulp configuration.
 
 var traceur = require('gulp-traceur');
+var _ = require('lodash');
 
-exports.traceur = function() {
-  return traceur({modules: 'amd', types: true, annotations: true});
+exports.traceur = function(options) {
+  return traceur(_.defaults(options || {}, {modules: 'amd', types: true, annotations: true}));
 };
