0a077a300664c5dfbc761996278c3120fc1a551d
Vojta Jina <vojta.jina@gmail.com>
2014-02-24 18:30:12 -0800

    Use assert

--- gulp.js
+++ gulp.js
@@ -4,5 +4,12 @@ var traceur = require('gulp-traceur');
 var _ = require('lodash');
 
 exports.traceur = function(options) {
-  return traceur(_.defaults(options || {}, {modules: 'amd', types: true, annotations: true}));
+  return traceur(_.defaults(options || {}, {
+    modules: 'amd',
+    types: true,
+    annotations: true,
+    typeAssertions: true,
+    typeAssertions: true,
+    typeAssertionModule: 'assert'
+  }));
 };
